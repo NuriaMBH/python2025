1e91994da37e94840492e783ef6af2095a12f99d
NuriaMBH <nuriamartin32@gmail.com>
2025-02-16 20:00:02 +0100

    Update python05positivo.py

--- python05positivo.py
+++ python05positivo.py
@@ -1,3 +1,12 @@
+print("ejemplo condicionales")
+valor= 2 # cambia el numero para ver que las variables funcionan
+#preguntamos 
+if(valor>=3):
+    #DENTRO DEL IF
+    print("positivo")
+else:
+    print("negativo")
+#####
 print("Número positivo/negativo/cero")
 print("Introduzca un numero")
 numero = int(input())
